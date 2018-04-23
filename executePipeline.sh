ad57fe7da002ede3591a73542bf64c00f92debf3
Unknown <samuelgodfreyhendrix@gmail.com>
2018-04-22 20:47:54 -0400

    Updated pipeline script

--- executePipeline.sh
+++ executePipeline.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-for file in registeredOMEs/*.ome.tif
+for dir in data/registeredOMEs/*
 do
-  justFilename=$(basename $file)
-
   mkdir data/out
-  ./bin/ZEBRA.exe "$file"
-  ./bin/NMF_GPU  data/out/${justFilename%%.*}/NNMF.nmf  -k 2  -j 10  -t 40  -i 20000
+  justDirname=$(basename $dir)
+  ./bin/ZEBRA.exe "${justDirname%%.*}" 512
+  ./bin/NMF_GPU  data/NNMF.nmf  -k 2  -j 10  -t 40  -i 20000
 
-  mkdir data/out/"${justFilename%%.*}"
-  mv data/pearsonArray.txt data/out/"${justFilename%%.*}"/"${justFilename%%.*}"_pearsonArray.txt
-  mv data/NNMF.nmf_H.txt data/out/"${justFilename%%.*}"/"${justFilename%%.*}"_H.txt
-  mv data/NNMF.nmf_W.txt data/out/"${justFilename%%.*}"/"${justFilename%%.*}"_W.txt
-  mv data/registeredOMEs/"${justFilename%%.*}"_TP1.tif data/out/"${justFilename%%.*}"/"${justFilename%%.*}"_TP1.tif
-  mv data/key.csv data/out/"${justFilename%%.*}"/key.csv
-  mv data/NNMF.nmf data/out/"${justFilename%%.*}"/NNMF.nmf
 
-  ./bin/NNMF_VISUALIZE.exe "${justFilename%%.*}" 2
-  mv data/RESULT.csv data/out/"${justFilename%%.*}"/RESULT.csv
+  mkdir data/out/"${justDirname%%.*}"
+  mv data/NNMF.nmf_H.txt data/out/"${justDirname%%.*}"/"${justDirname%%.*}"_H.txt
+  mv data/NNMF.nmf_W.txt data/out/"${justDirname%%.*}"/"${justDirname%%.*}"_W.txt
+  cp data/registeredOMEs/"${justDirname%%.*}"/"${justDirname%%.*}".ome0000.tif data/out/"${justDirname%%.*}"/
+  mv data/key.csv data/out/"${justDirname%%.*}"/key.csv
+  mv data/NNMF.nmf data/out/"${justDirname%%.*}"/NNMF.nmf
+
+  ./bin/NNMF_VISUALIZE.exe "${justDirname%%.*}" 2
+  mv data/RESULT.csv data/out/"${justDirname%%.*}"/RESULT.csv
 
 
   rm data/NNMF.csv

1ac62306a59a304b2f0abe8f7b77850e87eaa3d2
Unknown <samuelgodfreyhendrix@gmail.com>
2018-07-06 03:38:44 -0400

    Test

--- nnmf.cpp
+++ nnmf.cpp
@@ -1,12 +1,34 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 void multiplyMatrix(float* matrixA, float* matrixB, float* matrixC, int dimA,
      int dimCommon, int dimB);
 
 int main(int argc, char const *argv[]) {
 
+  int dimU = 3;
+  int dimS = 3;
+  int dimV = 3;
+
+  std::ifstream sMatrixFile("data/sMatrix.txt");
+
+  float* sMatrix = new float[dimS * dimS];
+  std::string currentLine = "";
+  int indexOfsMatrix = 0;
+  int numSZero = 0;
   
+  while (std::getline(sMatrixFile, currentLine)) {
+
+      std::istringstream ss(currentLine);
+      ss >> sMatrix[indexOfsMatrix];
+
+      //cout<<vtMatrix[indexOfvtMatrix]<<endl;
+
+      if(sMatrix[indexOfsMatrix] == 0.0f) ++numSZero;
+      indexOfsMatrix++;
+
+  }
 
   return 0;
 

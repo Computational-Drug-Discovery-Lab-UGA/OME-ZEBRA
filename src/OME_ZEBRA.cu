0cac77ac9cf021fcf2a1b4cf8c0f8a15cda7e866
Unknown <jop80923@uga.edu>
2018-07-27 00:35:14 -0400

    before doing make

--- src/OME_ZEBRA.cu
+++ src/OME_ZEBRA.cu
@@ -15,6 +15,36 @@ int main(int argc, char *argv[]) {
   uint32* tifVideo = readTiffVideo(baseDirectory, width, height, numTimePoints, baseName);
   float* normVideo = executeNormalization(tifVideo, width*height*numTimePoints);
 
+  std::string testImage = baseDirectory + "test.tif";
+  float max = std::numeric_limits<float>::min();
+  float min = std::numeric_limits<float>::max();
+  uint32* pixRow = new uint32[width];
+  for(int i = 0; i < height*width; ++i){
+    if(normVideo[i*numTimePoints + 2] < min){
+      min = normVideo[i*numTimePoints + 2];
+    }
+    if(normVideo[i*numTimePoints + 2] > max){
+      max = normVideo[i*numTimePoints + 2];
+    }
+  }
+  TIFF *tif = TIFFOpen(testImage.c_str(), "w");
+  if(tif){
+    TIFFSetField(tif, TIFFTAG_IMAGEWIDTH, width);
+    TIFFSetField(tif, TIFFTAG_IMAGELENGTH, height);
+    TIFFSetField(tif, TIFFTAG_SAMPLESPERPIXEL, 1);
+    TIFFSetField(tif, TIFFTAG_BITSPERSAMPLE, 32);
+
+    for(int row = 0; row < height; ++row){
+      for(int col = 0; col < width; ++col){
+        pixRow[col] =  (uint32) (UINT32_MAX*(normVideo[(row*width + col)*numTimePoints + 2] - min)/(max-min));
+      }
+      if (TIFFWriteScanline(tif, pixRow, row, 0) != 1) {
+        std::cout << "ERROR WRITING FIRST TIMEPOINT" << std::endl;
+        exit(-1);
+      }
+    }
+  }
+
 
 
   delete[] tifVideo;

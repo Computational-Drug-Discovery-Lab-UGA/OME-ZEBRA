58e09675d2392b15a97de03277ade5fe59033f59
Unknown <jop80923@uga.edu>
2018-08-26 17:05:13 -0400

    whoops

--- node_test.sh
+++ node_test.sh
@@ -6,13 +6,13 @@ make clean
 make
 
 ./bin/ZEBRA_NMF "../node_zebrafish_test/registered20161028/registered gad1b+,- heterozygotic_2/registeredfish2_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/" 100 0
-./bin/NMF_GPU  ../path/to/tifs/NNMF.nmf  -k 100 -j 10 -t 40  -i 20000
+./bin/NMF_GPU   "../node_zebrafish_test/registered20161028/registered gad1b+,- heterozygotic_2/registeredfish2_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/NNMF.txt"  -k 100 -j 10 -t 40  -i 20000
 ./bin/ZEBRA_NMF "../node_zebrafish_test/registered20161028/registered gad1b+,- heterozygotic_2/registeredfish2_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/" 100 1
 
 ./bin/ZEBRA_NMF "../node_zebrafish_test/registered20161028/registered gad1b+,- heterozygotic_3/registeredfish3_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/" 100 0
-./bin/NMF_GPU  ../path/to/tifs/NNMF.nmf  -k 100 -j 10 -t 40  -i 20000
+./bin/NMF_GPU   "../node_zebrafish_test/registered20161028/registered gad1b+,- heterozygotic_3/registeredfish3_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/NNMF.txt"  -k 100 -j 10 -t 40  -i 20000
 ./bin/ZEBRA_NMF "../node_zebrafish_test/registered20161028/registered gad1b+,- heterozygotic_3/registeredfish3_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/" 100 1
 
 ./bin/ZEBRA_NMF "../node_zebrafish_test/registered20161028/registered gad1b wild type/registeredfish1_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/" 100 0
-./bin/NMF_GPU  ../path/to/tifs/NNMF.nmf  -k 100 -j 10 -t 40  -i 20000
+./bin/NMF_GPU   "../node_zebrafish_test/registered20161028/registered gad1b wild type/registeredfish1_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/NNMF.txt"  -k 100 -j 10 -t 40  -i 20000
 ./bin/ZEBRA_NMF "../node_zebrafish_test/registered20161028/registered gad1b wild type/registeredfish1_5d_gcamp5g_gad1b_rfp_WTorHet_25fps_5mw_488(1)/" 100 1

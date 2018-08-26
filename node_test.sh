b19d5d2ade86afa1c52527485910895e6f85a8d4
Unknown <jop80923@uga.edu>
2018-08-26 16:20:16 -0400

    update to bash

--- node_test.sh
+++ node_test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-/bin/ZEBRA_NMF ../ 100 0
-./bin/NMF_GPU  data/NNMF.nmf  -k 100 -j 5  -t 20  -i 20000
-/bin/ZEBRA_NMF ../ 100 1
+/bin/ZEBRA_NMF ../path/to/tifs/ 100 0
+./bin/NMF_GPU  ../path/to/tifs/NNMF.nmf  -k 100 -j 10 -t 40  -i 20000
+/bin/ZEBRA_NMF ../path/to/tifs/ 100 1
+
+/bin/ZEBRA_NMF ../path/to/tifs/ 100 0
+./bin/NMF_GPU  ../path/to/tifs/NNMF.nmf  -k 100 -j 10 -t 40  -i 20000
+/bin/ZEBRA_NMF ../path/to/tifs/ 100 1
+
+/bin/ZEBRA_NMF ../path/to/tifs/ 100 0
+./bin/NMF_GPU  ../path/to/tifs/NNMF.nmf  -k 100 -j 10 -t 40  -i 20000
+/bin/ZEBRA_NMF ../path/to/tifs/ 100 1
